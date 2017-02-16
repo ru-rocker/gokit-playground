9e31a1bb61e703c8ad241cf7aa4d49819782226c
ru.rocker81@gmail.com <ru.rocker81@gmail.com>
2017-02-16 16:16:58 +0700

    add lorem service

---
+++ lorem/endpoints.go
@@ -0,0 +1,38 @@
+package lorem
+
+import (
+	"github.com/go-kit/kit/endpoint"
+	"context"
+	"strings"
+)
+
+type Endpoints struct {
+	LoremEndpoint endpoint.Endpoint
+}
+
+func MakeLoremEndpoint(svc Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(LoremRequest)
+
+		var (
+			txt string
+			min, max int
+		)
+
+		min = req.Min
+		max = req.Max
+
+		if strings.EqualFold(req.RequestType, "Word") {
+			txt = svc.Word(min, max)
+		} else if strings.EqualFold(req.RequestType, "Sentence"){
+			txt = svc.Sentence(min, max)
+		} else if strings.EqualFold(req.RequestType, "Paragraph") {
+			txt = svc.Paragraph(min, max)
+		} else {
+			return nil, ErrRequestTypeNotFound
+		}
+
+		return LoremResponse{Message: txt}, nil
+	}
+
+}
---
+++ lorem/lorem.d/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"os"
+	"os/signal"
+	"syscall"
+	"net/http"
+	"fmt"
+	"github.com/ru-rocker/gokit-playground/lorem"
+	"github.com/go-kit/kit/log"
+)
+
+func main() {
+	ctx := context.Background()
+	errChan := make(chan error)
+
+	var svc lorem.Service
+	svc = lorem.LoremService{}
+	endpoint := lorem.Endpoints{
+		LoremEndpoint: lorem.MakeLoremEndpoint(svc),
+	}
+
+	// Logging domain.
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
+		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
+	}
+
+	r := lorem.MakeHttpHandler(ctx, endpoint, logger)
+
+	// HTTP transport
+	go func() {
+		fmt.Println("Starting server at port 8080")
+		handler := r
+		errChan <- http.ListenAndServe(":8080", handler)
+	}()
+
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+	fmt.Println(<- errChan)
+}
